82cd822126d8b1dbe9a1e1fcfad5f3553a5eb743
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-11-21 15:57:00 +0100

    Init app

---
+++ gridcapa-core-valid-day-ahead-conservative-post-processing-app/src/main/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative_post_processing/CoreValidD2ConservativePostProcessingApplication.java
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2025, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative_post_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("hideutilityclassconstructor")
+@SpringBootApplication
+public class CoreValidD2ConservativePostProcessingApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(CoreValidD2ConservativePostProcessingApplication.class, args);
+    }
+
+}
