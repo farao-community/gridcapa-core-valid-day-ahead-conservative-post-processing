d1a4cee9d2dd6c53e2c54605a4c537bf388f8a2e
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-12-08 10:03:35 +0100

    Update for improved test coverage

--- gridcapa-core-valid-day-ahead-conservative-post-processing-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative_post_processing/services/PostProcessingServiceTest.java
+++ gridcapa-core-valid-day-ahead-conservative-post-processing-app/src/test/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative_post_processing/services/PostProcessingServiceTest.java
@@ -4,7 +4,7 @@
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
  *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative_post_processing;
+package com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative_post_processing.services;
 
 import com.farao_community.farao.gridcapa.task_manager.api.ProcessFileDto;
 import com.farao_community.farao.gridcapa.task_manager.api.ProcessFileStatus;
@@ -12,7 +12,7 @@ import com.farao_community.farao.gridcapa.task_manager.api.ProcessRunDto;
 import com.farao_community.farao.gridcapa.task_manager.api.TaskDto;
 import com.farao_community.farao.gridcapa.task_manager.api.TaskParameterDto;
 import com.farao_community.farao.gridcapa.task_manager.api.TaskStatus;
-import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative_post_processing.services.PostProcessingService;
+import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative_post_processing.CoreValidD2PostProcessingConstants;
 import com.farao_community.farao.minio_adapter.starter.MinioAdapter;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -23,21 +23,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.RestTemplate;
-import reactor.core.publisher.Flux;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
-class CoreValidD2PostProcessingHandlerTest {
+class PostProcessingServiceTest {
 
     @Autowired
-    private CoreValidD2PostProcessingHandler coreValidD2PostProcessingHandler;
+    private PostProcessingService postProcessingService;
 
     @MockitoBean
     private RestTemplateBuilder restTemplateBuilder;
@@ -45,16 +47,10 @@ class CoreValidD2PostProcessingHandlerTest {
     @MockitoBean
     private MinioAdapter minioAdapter;
 
-    @MockitoBean
-    private PostProcessingService postProcessingService;
-
     @Test
     void consumeTaskDtoUpdateOK() {
-        Mockito.reset(postProcessingService);
-        final TaskDto taskDto = getTestTaskDto(true);
-        final TaskDto[] tasks = {taskDto};
-        final Flux<TaskDto> taskDtoFlux = Flux.fromStream(Stream.of(taskDto));
-        final Consumer<Flux<TaskDto>> consumer = coreValidD2PostProcessingHandler.consumeTaskDtoUpdate();
+        final TaskDto taskDto = getTestTaskDto();
+        final Set<TaskDto> tasks = Set.of(taskDto);
         final RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
         final ResponseEntity response1 = Mockito.mock(ResponseEntity.class);
         Mockito.when(response1.getStatusCode()).thenReturn(HttpStatus.OK);
@@ -65,40 +61,19 @@ class CoreValidD2PostProcessingHandlerTest {
         Mockito.when(restTemplate.getForEntity("http://test-dummy/tasks/businessdate/2025-11-28/allOver", Boolean.class)).thenReturn(response1);
         Mockito.when(restTemplate.getForEntity("http://test-dummy/tasks/businessdate/2025-11-28", TaskDto[].class)).thenReturn(response2);
         Mockito.when(restTemplateBuilder.build()).thenReturn(restTemplate);
-        consumer.accept(taskDtoFlux);
-        Mockito.verify(postProcessingService, Mockito.atLeastOnce()).processTasks(Mockito.any(LocalDate.class), Mockito.any());
+        try (InputStream in = getClass().getResource("/testBranchIvaFile.json").openStream()) {
+            Mockito.when(minioAdapter.getFileFromFullPath("testFilePath")).thenReturn(in);
+            postProcessingService.processTasks(LocalDate.of(2025, 11, 28), tasks);
+        } catch (IOException e) {
+            fail();
+        }
     }
 
-    @Test
-    void consumeTaskDtoUpdateNotAllFinished() {
-        Mockito.reset(postProcessingService);
-        final TaskDto taskDto = getTestTaskDto(true);
-        final Flux<TaskDto> taskDtoFlux = Flux.fromStream(Stream.of(taskDto));
-        final Consumer<Flux<TaskDto>> consumer = coreValidD2PostProcessingHandler.consumeTaskDtoUpdate();
-        final RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
-        final ResponseEntity response1 = Mockito.mock(ResponseEntity.class);
-        Mockito.when(response1.getStatusCode()).thenReturn(HttpStatus.OK);
-        Mockito.when(response1.getBody()).thenReturn(Boolean.FALSE);
-        Mockito.when(restTemplate.getForEntity("http://test-dummy/tasks/businessdate/2025-11-28/allOver", Boolean.class)).thenReturn(response1);
-        consumer.accept(taskDtoFlux);
-        Mockito.verify(postProcessingService, Mockito.never()).processTasks(Mockito.any(LocalDate.class), Mockito.any());
-    }
-
-    @Test
-    void consumeTaskDtoUpdateNotFinished() {
-        Mockito.reset(postProcessingService);
-        final TaskDto taskDto = getTestTaskDto(false);
-        final Flux<TaskDto> taskDtoFlux = Flux.fromStream(Stream.of(taskDto));
-        final Consumer<Flux<TaskDto>> consumer = coreValidD2PostProcessingHandler.consumeTaskDtoUpdate();
-        consumer.accept(taskDtoFlux);
-        Mockito.verify(postProcessingService, Mockito.never()).processTasks(Mockito.any(LocalDate.class), Mockito.any());
-    }
-
-    private static TaskDto getTestTaskDto(final boolean isOver) {
+    private static TaskDto getTestTaskDto() {
         final OffsetDateTime timestamp = OffsetDateTime.of(2025, 11, 28, 12, 0, 0, 0, ZoneOffset.UTC);
         return new TaskDto(UUID.randomUUID(),
                            timestamp,
-                           isOver ? TaskStatus.SUCCESS : TaskStatus.CREATED,
+                           TaskStatus.SUCCESS,
                            List.of(),
                            List.of(),
                            List.of(new ProcessFileDto("testFilePath", "IVA-RESULT", ProcessFileStatus.VALIDATED, "tesFileName", "testDocId", timestamp)),
